233966b3d24cc29fec3ca8030a646fd2144cd3e8
Seema <seemaullal@gmail.com>
2015-07-29 11:42:59 -0400

    Added loader and changed styling a bit

--- client/pre-build/postDetails/datePicker.directive.js
+++ client/pre-build/postDetails/datePicker.directive.js
@@ -6,10 +6,12 @@ angular.module('kargo')
       link: function(scope,element, attr) {
         scope.pickDate = function() {
           // to get value of date picker directive
+          scope.inProgress = true;
           scope.date1 = document.getElementsByTagName('input')[0].value;
           scope.date2 = document.getElementsByTagName('input')[1].value;
           Analytics.getDateRange(scope.date1, scope.date2).then(function(trackerData) {
             scope.data = trackerData;
+            scope.inProgress = false;
           });
         };
       }
